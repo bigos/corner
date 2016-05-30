e361e2c5ffd4a248197d448e6bfc05a111f0cc7d
Jacek Podkanski <ruby.object@googlemail.com>
2016-05-30 16:13:11 +0100

    tweaking views

--- resources/views/contact_messages/index.blade.php
+++ resources/views/contact_messages/index.blade.php
@@ -6,4 +6,22 @@
   This is my /resources/views/projects/index.blade.php file!
 
   <p>Hello, <?php echo $contact_messages; ?></p>
+  <ul>
+  @foreach( $contact_messages as $msg )
+
+    <li>
+      {{ $msg->id }}
+    </li>
+    <li>
+      {{ $msg->subject }}
+    </li>
+    <li>
+      {{ $msg->message }}
+    </li>
+    <li>
+      {{ $msg }}
+    </li>
+    <hr>
+  @endforeach
+  </ul>
 @endsection
