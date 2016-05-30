1ad19ac0422a9cc8daf35a7e640e6031b6adcbe4
Jacek Podkanski <ruby.object@googlemail.com>
2016-05-30 14:39:52 +0100

    first successful migration in laravel

---
+++ database/migrations/2016_05_30_133045_create_contact_messages.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateContactMessages extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contact_messages', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('subject');
+            $table->text('message');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('contact_messages');
+    }
+}
